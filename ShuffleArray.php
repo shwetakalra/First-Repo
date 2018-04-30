458ef2081623ea66118417fb22b22a968d9a1ab9
shwetakalra <shwetakalra92@gmail.com>
2018-04-30 10:55:53 +0530

    new file shuffle

---
+++ ShuffleArray.php
@@ -0,0 +1,14 @@
+//shuffle an array
+<?php 
+$arr = array(2,4,1,9,8);
+$array1= $arr;
+for($i=0;$i<(count($arr)-2);$i++)
+{
+    print_r($array1);
+    $ran_val = array_rand($array1);
+    array_pop($array1);
+    
+    $arr[$ran_val] = $arr[(count($arr)-1)]; 
+    $arr[(count($arr)-1)] = $ran_val;
+}
+?>
